e7460b9e1d26d0f5809f0b5a7c9076cb54746e20
markdawkins <44737733+markdawkins@users.noreply.github.com>
2022-12-07 20:24:38 -0500

    Add files via upload

---
+++ python_scripts/Loopscript.py
@@ -0,0 +1,7 @@
+# Define a list of two variables
+my_list = [('apple', 'fruit'), ('carrot', 'vegetable')]
+
+# Loop through the list and print each variable
+for item in my_list:
+    print(item[0], item[1])
+
