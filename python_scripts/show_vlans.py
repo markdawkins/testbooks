85f0626817589f4fa7f5aec5362a778657e24c66
markdawkins <44737733+markdawkins@users.noreply.github.com>
2023-03-09 13:14:49 -0500

    Update show_vlans.py

--- python_scripts/show_vlans.py
+++ python_scripts/show_vlans.py
@@ -1,13 +1,16 @@
-#!/usr/bin/env python
 from netmiko import ConnectHandler
+from netmiko import NetMikoTimeoutException
+from paramiko.ssh_exception import SSHException
+from netmiko import NetMikoAuthenticationException
+
 import time
 from getpass import getpass
 
 LIST = input("Enter List Name:")
-password = getpass("Enter password: ")
+#password = getpass("Enter password: ")
 REPORT = input("Enter Report Name:")
-reportname = "./REPORTS/VLAN_REPORTS/%s.csv" % REPORT
-listname = "./LISTS/%s.txt"  % LIST
+reportname = "\REPORTS\VLAN_REPORTS\%s.csv" % REPORT
+listname = "\LISTS\%s.txt"  % LIST
 
 print ("Writing output to REPORTS directory...")
 time.sleep(1)
@@ -21,20 +24,40 @@ for line in f:
     iosv_l2 = {
              'device_type':'cisco_ios',
              'ip':host,
-             'username':'admin',
-             'password':password,
-           }
+             'username':'svc_ansible',
+             'password':'NhYadAhWE4PQ9X(wz!',
+             'timeout': 60
+    }
 
-    net_connect = ConnectHandler(**iosv_l2)
+    try:
+            net_connect = ConnectHandler(**iosv_l2)
+    except (NetMikoAuthenticationException):
+           print ('Authentication failure: ' + host )
+           continue
+    except (NetMikoTimeoutException):
+           print ('Timeout to device: ' + host)
+           file.write ('Timeout to device: ' + host)
+           continue
+    except (EOFError):
+           print ('End of file while attempting device ' + host)
+           file.write ('End of file while attempting device ' + host)
+           continue
+    except (SSHException):
+           print ('SSH Issue. Are you sure SSH is enabled? ' + host)
+           file.write ('SSH Issue. Are sure SSH is enabled? ' + host)
+           continue
+    except Exception as unknown_error:
+           print ('some other error: '+ str(unknown_error) )
+           file.write('Some other error: ' + str(unknown_error))
+           continue
+
+    output0 = net_connect.send_command('show run | inc hostname' , read_timeout=30)
+    time.sleep(2)
     print(host)
-
-    output0 = net_connect.send_command('show run | inc hostname')
     print(output0[9:25])
     file.write(output0[9:25])
 
-    output1 = net_connect.send_command('show vlan ID 20-25')
+    output1 = net_connect.send_command('show vlan ID 20-25' , read_timeout=30)
     print(output1)
-    file.write(host)
     file.write (output1)
-    net_connect.send_command('end\n')
     time.sleep(1)
